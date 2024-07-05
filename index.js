c0186c33314353fd99d633ee2af2c4528654b142
rjspyk5 <rjspyk5@gmail.com>
2024-07-06 01:37:41 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require("cors");
+require("dotenv").config();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("test");
+});
+
+app.listen(port, () => {
+  console.log("server running");
+});
