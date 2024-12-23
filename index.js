4de2488f320e865ff1f7f80c9c0605603ceab9f5
rjspyk5 <rjspyk5@gmail.com>
2024-12-23 22:50:02 +0600

    remove message controllar

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const port = process.env.PORT ?? 3000;
 const cors = require("cors");
 const connectDb = require("./config/config");
+
 app.use(express.json());
 app.use(
   cors({
