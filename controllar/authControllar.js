b1ead6e34c6ed877d5f7407a8946b0ebaef0c3a3
rjspyk5 <rjspyk5@gmail.com>
2025-01-06 00:15:44 +0600

    jwt setup complete

--- controllar/authControllar.js
+++ controllar/authControllar.js
@@ -1,11 +1,32 @@
 const { login } = require("../model/authModel");
+require("dotenv").config();
+var jwt = require("jsonwebtoken");
 
 module.exports = {
   login: async (req, res) => {
-    const email = req.query.email;
-    const result = await login(email);
+    const { email, password } = req.body;
+    try {
+      const result = await login(email);
+      if (!result) return res.status(200).send({ message: "User not found" });
 
-    console.log(result);
-    return result;
+      if (result?.password !== password)
+        return res
+          .status(200)
+          .send({ message: "Password didn't match,try again later" });
+
+      const token = jwt.sign(
+        {
+          email: result.email,
+        },
+        process.env.TOKENSECRET,
+        { expiresIn: "30d" }
+      );
+
+      return res.status(200).send({ message: "Login Successfull", token });
+    } catch (error) {
+      return res
+        .status(400)
+        .send({ message: "Server having some issues", error });
+    }
   },
 };
