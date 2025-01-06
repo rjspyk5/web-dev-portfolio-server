3478a95903125c127a583ecfbc5ef5aa7471c160
rjspyk5 <rjspyk5@gmail.com>
2025-01-07 00:34:41 +0600

    project controller response message set

--- controllar/projectControllar.js
+++ controllar/projectControllar.js
@@ -9,31 +9,42 @@ const {
 module.exports = {
   uploadProjects: async (req, res) => {
     const data = req?.body;
-
-    const result = await uploadProjectsModel(data);
-
-    res.send(result);
+    try {
+      const result = await uploadProjectsModel(data);
+      return res.send({ message: "Project upload successfully" });
+    } catch (error) {
+      return res.send({ message: "Something went wrong", error });
+    }
   },
   getProjects: async (req, res) => {
     const result = await getAllProjects();
-    res.send(result);
+    return res.send(result);
   },
   getProject: async (req, res) => {
     const id = req.params.id;
-
     const result = await getProject(id);
-    res.send(result);
+    return res.send(result);
   },
   deleteProjects: async (req, res) => {
     const id = req.params.id;
-    const result = await deleteProject(id);
-
-    res.send(result);
+    try {
+      const result = await deleteProject(id);
+      return res.send({ message: "Project deleted successfully" });
+    } catch (error) {
+      return res.send({ message: "Something went wrong", error });
+    }
   },
+
   updateProjects: async (req, res) => {
     const id = req.params.id;
     const data = req?.body;
-    const result = await updateProject(id, data);
-    res.send(result);
+    try {
+      const result = await updateProject(id, data);
+      if (result?.modifiedCount) {
+        return res.send({ message: "Project Update Successfully" });
+      }
+    } catch (error) {
+      return res.send({ message: "Something went wrong", error });
+    }
   },
 };
